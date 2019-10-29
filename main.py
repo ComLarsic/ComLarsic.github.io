36cd7226403c72245687f446bc20b3d32fc328bb
ComLarsic <larsjanjensen@gmail.com>
2019-10-29 08:32:47 -0400

    ComLarsic

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from flask import *
 
 app = Flask(__name__)
 Home = "/"
+about = "/about"
+contact = "/contact"
+category = "/Scategory"
 
 
 #Website routes
@@ -10,7 +13,17 @@ Home = "/"
 def home():
     return render_template('home.html')
 
+@app.route(about)
+def about():
+    return render_template("about.html")
 
+@app.route(contact)
+def contact():
+    return render_template("contact.html")
+
+@app.route(category):
+def category():
+    return render_template("category.html")
 
 
 if (__name__ == "__main__"):
