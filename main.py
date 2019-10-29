0e13c07db0354928fbc9ca771d7c2dacbd10659f
Rhydium <tijn.groenen@gmail.com>
2019-10-29 12:19:01 +0100

    Filesssssssss
    Yeah boi!

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from flask import *
 
 
 app = Flask(__name__)
+Home = "/"
 
 
-@app.route("/")
+#Website routes
+@app.route(Home)
 def home():
     return render_template('home.html')
 
-if __name__ == "__main__":
+
+
+
+if (__name__ == "__main__"):
     app.run(debug=True)
+app.run(host='0.0.0.0', port=8080)
